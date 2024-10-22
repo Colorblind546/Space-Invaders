60e91e184871690bdc210aafa2add2e5b1a749bd
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2024-10-22 11:07:59 +0200

    Shift can be pressed to move at half speed, for more control

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,56 @@ public class PlayerMovement : MonoBehaviour
 
     // Player moveSpeed
     public float moveSpeed;
+    float baseSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseSpeed = moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Moves player at half speed, for more fine movement
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = baseSpeed / 2;
+        }
+        else
+        {
+            moveSpeed = baseSpeed;
+        }
+
         // Moves left if 'A' is held, and right if 'D' is held
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            MoveLeft();
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+            MoveRight();
         }
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -14.1875f, 14.1875f), transform.position.y, 0);
         
 
     }
 
+    /// <summary>
+    /// Moves player left
+    /// </summary>
+    void MoveLeft()
+    {
+        transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+    /// <summary>
+    /// Moves player right
+    /// </summary>
+    void MoveRight()
+    {
+        transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+    }
 
     
 }
