e859f3cfa233c0a73c2971bef51199fb784fd0c9
Colorblind546 <morgan.marinbeishline@elev.ga.lbs.se>
2024-10-25 12:13:57 +0200

    Invaders not moving or shooting when restarting main game scene fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -47,7 +47,6 @@ public class GameManager : MonoBehaviour
     // Kollar om GameManager redan finns n�r spelet startar, om det g�r det s� f�rst�rs det. Annars s� g�r det s� att den inte f�rst�rs n�r den laddar. 
 
     private void Awake()
-        
     {
         
         position = transform.position; // This is never used, should we remove it?
@@ -66,7 +65,12 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    { 
+    {
+        if (invaders == null)
+        {
+            invaders = GameObject.FindGameObjectWithTag("Invaders").GetComponent<Invaders>();
+        }
+        
         // Sets direction to right 
         direction = Vector3.right;
     }
@@ -80,52 +84,55 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
-
-
-        foreach (GameObject ob in invaders.Invaderss) // Best�mmer vad varje invader gameobject ska g�ra 
+        if (invaders == null)
         {
-            // skjuter en laser rakt ner som kollar om n�got �r i v�gen
-
-            RaycastHit2D hitcheck = Physics2D.Raycast(ob.transform.position - Vector3.up, -Vector2.up, 10f, invaderLayer);
-
-            Invader invader = ob.GetComponent<Invader>();
-
-
-            if (hitcheck.collider == null && !invader.cooldown && invader.invaderexist == true)
-            {
-
-                   Instantiate(invaderLaser, ob.transform.position, Quaternion.identity);
-                   invader.InvaderCooldown();
-                   
-               
-              
-
-            }
-
-            // Invaders �ker fr�n sida till sida
-
-            float speed = 1f;
-            ob.transform.position += speed * Time.deltaTime * direction;
-
-            Vector3 rightwall = new Vector3(15, -15, -10);
-            //Camera.main.ViewportToWorldPoint(Vector3.right);
-
-            Vector3 leftwall = new Vector3(-15, -15, -10);
-            //Camera.main.ViewportToWorldPoint(Vector3.zero);
-
-            if (direction == Vector3.right && ob.transform.position.x >= rightwall.x - 1f)
-            {
-                print(rightwall + ", and " + leftwall);
-                Advance();
-                break;
-            }
-            else if (direction == Vector3.left && ob.transform.position.x <= leftwall.x + 1f)
-            {
-                Advance();
-                break;
-            }
-
+            FindInvaders();
         }
+
+        if (invaders != null)
+        {
+            foreach (GameObject ob in invaders.Invaderss) // Best�mmer vad varje invader gameobject ska g�ra 
+            {
+                // skjuter en laser rakt ner som kollar om n�got �r i v�gen
+
+                RaycastHit2D hitcheck = Physics2D.Raycast(ob.transform.position - Vector3.up, -Vector2.up, 10f, invaderLayer);
+
+                Invader invader = ob.GetComponent<Invader>();
+
+
+                if (hitcheck.collider == null && !invader.cooldown && invader.invaderexist == true)
+                {
+
+                    Instantiate(invaderLaser, ob.transform.position, Quaternion.identity);
+                    invader.InvaderCooldown();
+                }
+
+                // Invaders �ker fr�n sida till sida
+
+                float speed = 1f;
+                ob.transform.position += speed * Time.deltaTime * direction;
+
+                Vector3 rightwall = new Vector3(15, -15, -10);
+                //Camera.main.ViewportToWorldPoint(Vector3.right);
+
+                Vector3 leftwall = new Vector3(-15, -15, -10);
+                //Camera.main.ViewportToWorldPoint(Vector3.zero);
+
+                if (direction == Vector3.right && ob.transform.position.x >= rightwall.x - 1f)
+                {
+                    print(rightwall + ", and " + leftwall);
+                    Advance();
+                    break;
+                }
+                else if (direction == Vector3.left && ob.transform.position.x <= leftwall.x + 1f)
+                {
+                    Advance();
+                    break;
+                }
+
+            }
+        }
+        
 
 
     }
@@ -147,5 +154,21 @@ public class GameManager : MonoBehaviour
             invaderObj.transform.position = position;
         }
         
+    }
+
+    /// <summary>
+    /// Finds Invaders in scene by finding Invaders tag, and assigns it to invaders
+    /// </summary>
+    void FindInvaders()
+    {
+        if (invaders == null)
+        {
+            GameObject invadersObj = GameObject.FindGameObjectWithTag("Invaders");
+            if (invadersObj != null)
+            {
+                invaders = invadersObj.GetComponent<Invaders>();
+            }
+            
+        }
     }
 }
